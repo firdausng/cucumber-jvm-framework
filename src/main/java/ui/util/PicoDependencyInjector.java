47c4d72e01d3aeb0b3456d1c990b4b001aec7211
Kamaruddin, Firdaus <firdaus.kamaruddin@mbww.com>
2017-11-21 20:12:36 +0800

    - initial commit

---
+++ src/main/java/ui/util/PicoDependencyInjector.java
@@ -0,0 +1,10 @@
+package ui.util;
+
+import cucumber.runtime.java.picocontainer.PicoFactory;
+
+public class PicoDependencyInjector extends PicoFactory {
+
+    public PicoDependencyInjector() {
+        addClass(SharedDriver.class);
+    }
+}
